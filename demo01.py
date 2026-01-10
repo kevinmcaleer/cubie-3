4dcd546c333c1e9d774b0222525945523eecbe77
Kevin McAleer <kevinmcaleer@gmail.com>
2026-01-10 16:19:37 +0000

    made speed and time changes

--- demo01.py
+++ demo01.py
@@ -5,33 +5,33 @@ async def main():
     cubie3 = Cubie3()
 
     # Forward
-    await cubie3.forward(0.6)
-    await asyncio.sleep(2)
+    await cubie3.forward(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
     # Backward
-    await cubie3.backward(0.6)
-    await asyncio.sleep(2)
+    await cubie3.backward(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
     # Strafe left
-    await cubie3.strafe_left(0.6)
-    await asyncio.sleep(2)
+    await cubie3.strafe_left(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
     # Strafe right
-    await cubie3.strafe_right(0.6)
-    await asyncio.sleep(2)
+    await cubie3.strafe_right(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
     # Rotate left
-    await cubie3.rotate_left(0.6)
-    await asyncio.sleep(2)
+    await cubie3.rotate_left(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
     # Rotate right
-    await cubie3.rotate_right(0.6)
-    await asyncio.sleep(2)
+    await cubie3.rotate_right(0.2)
+    await asyncio.sleep(0.5)
     await cubie3.stop()
 
 if __name__ == "__main__":
